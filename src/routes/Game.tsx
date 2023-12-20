70348074ae543bc66dcb396ab16494408bc59138
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-12-20 10:33:21 +0100

    timer with pause

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useNavigate } from "react-router"
 
 const getRandomPosition = () => {
@@ -9,23 +9,61 @@ const getRandomPosition = () => {
 }
 
 export default function Home() {
+    const [start, setStart] = useState(0)
+    const [pause, setPause] = useState(false)
+    const [totalTime, setTotalTime] = useState(0)
+    const [intervalId, setIntervalId] = useState(0)
+    const [_, refresh] = useState(0)
     const [position, setPosition] = useState(getRandomPosition())
     const [nbKilledDivs, setNbKilledDivs] = useState(0)
+    
     const navigate = useNavigate()
 
+    const timeValue = totalTime + (pause ? 0 : (Date.now() - start) / 1000)
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            refresh(Math.random())
+        }, 120)
+        setIntervalId(id)
+        setStart(Date.now())
+    }, [])
+
     const handleDivClick = () => {
-        setPosition(getRandomPosition())
-        if(nbKilledDivs < 9 ){
-            setNbKilledDivs(nbKilledDivs + 1)
+        if(!pause){
+            setPosition(getRandomPosition())
+            if(nbKilledDivs < 9 ){
+                setNbKilledDivs(nbKilledDivs + 1)
+            }
+            else {
+                clearInterval(intervalId)
+                navigate('/end')
+            }
         }
-        else {
-            navigate('/end')
-        }
+    }
+
+    const handleRestart = () => {
+        setPause(false)
+        setStart(Date.now())
+    }
+
+    const handlePause = () => {
+        setPause(true)
+        setTotalTime(timeValue)
     }
 
     return (
         <div className="page game-page">
             <div className="kill-cpt">{nbKilledDivs}/10</div>
+            <div className="timer">
+                <div className="timer-value">{timeValue.toFixed(3)} sec</div>
+                <div className="timer-actions">
+                    {pause 
+                        ? <button className="restart-btn" onClick={handleRestart}>Restart</button>
+                        : <button className="pause-btn" onClick={handlePause}>Pause</button>
+                    }
+                </div>
+            </div>
             <div 
                 className="div-to-kill" 
                 style={position}
