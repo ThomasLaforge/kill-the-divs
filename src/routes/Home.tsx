592dad5812f338e6e50473c7f31d6848a57f5107
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-12-20 14:19:04 +0100

    pwa install

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 
 export default function Home() {
     const navigate = useNavigate()
 
+    const [installedPWA, setInstalledPWA] = useState(false)
+    const [eventPWAInstall, setEventPrompt] = useState<BeforeInstallPromptEvent | null>(null)
+
+    useEffect(() => {
+        window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault()
+            setEventPrompt(e as BeforeInstallPromptEvent)
+        })
+
+        window.addEventListener('appinstalled', () => {
+            setInstalledPWA(true)
+        })
+    }, [])
+
+
     return (
         <div className="page home-page">
             <button onClick={() => navigate('/game')}>Start</button>
+            {!installedPWA ? <button onClick={() => eventPWAInstall?.prompt()}>Installer</button> : null}
         </div>
     )
 }
